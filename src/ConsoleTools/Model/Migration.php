d99c9e28db7c14d1654f087275eb3e53e6353d2b
Vadim Leontiev <vadim@centralpowertrading.com>
2013-10-03 10:22:19 +0300

    Fixed migration type in migration table

--- src/ConsoleTools/Model/Migration.php
+++ src/ConsoleTools/Model/Migration.php
@@ -52,7 +52,7 @@ class Migration
     {
         $sql = "
             CREATE TABLE IF NOT EXISTS `".self::TABLE."`(
-                `migration` int NOT NULL
+                `migration` VARCHAR(20) NOT NULL
             ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
         ";
 
